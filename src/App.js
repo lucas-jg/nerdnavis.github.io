636811d4ff45e739736f8519a64a4019f8bb2d3a
Lucas <lucas_backend@kakao.com>
2024-08-07 15:23:07 +0900

    chore: Update logo size and change page title

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import imgfile from "./resources/8eb05fdf62801b9d.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={imgfile} className="App-logo" alt="logo" />
+        <h1>Coming Soon...</h1>
       </header>
     </div>
   );
